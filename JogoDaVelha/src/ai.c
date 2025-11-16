8c8957d6e79601bfd37a40dab53fcf6e06b66ea1
Largyrixa <lopes.m.iago@gmail.com>
2025-11-15 22:50:55 -0300

    Adicionado a 'FreeTree'

--- JogoDaVelha/src/ai.c
+++ JogoDaVelha/src/ai.c
@@ -9,8 +9,51 @@ struct Node
   struct Node *Sons[MAX_SONS];
 };
 
-void InitTree(GameTree *root)
+GameTree *CreateTree(char BoardState, char Player)
 {
-  root = NULL;
+  GameTree *NewTree = malloc(sizeof(GameTree));
+  NewTree->BoardState = BoardState;
+  NewTree->Player = Player;
+
+  for (int i = 0; i < MAX_SONS; i++)
+  {
+    NewTree->Sons[i] = NULL;
+  }
 }
 
+bool AddNode(GameTree *root, char BoardState, int index)
+{
+  if (root == NULL)
+  {
+    return false;
+  }
+
+  char NewPlayer;
+
+  switch (root->Player)
+  {
+  case 'X':
+    NewPlayer = 'O';
+    break;
+
+  case 'O':
+    NewPlayer = 'X';
+    break;
+  }
+
+  GameTree *NewNode = CreateTree(BoardState, NewPlayer);
+  root->Sons[index] = NewNode;
+}
+
+void FreeTree(GameTree *root)
+{
+  if (root == NULL)
+    return;
+
+  for (int i = 0; i < MAX_SONS; i++)
+  {
+    FreeTree(root->Sons[i]);
+  }
+
+  free(root);
+}
