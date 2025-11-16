e741261b3d2a5a8158147e1f0a1b9b858ac7c9a9
Largyrixa <lopes.m.iago@gmail.com>
2025-11-16 17:25:03 -0300

    Adiciona o srand na main

--- JogoDaVelha/src/main.c
+++ JogoDaVelha/src/main.c
@@ -5,8 +5,12 @@
 #include "raylib.h"
 #include "ai.h"
 
+#include <stdlib.h>
+#include <time.h>
+
 int main()
 {
+  srand(time(NULL));
   // Inicialização da janela do jogo.
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Jogo da Velha");
   SetTargetFPS(60);
